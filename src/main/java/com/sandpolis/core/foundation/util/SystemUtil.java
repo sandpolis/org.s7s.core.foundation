86c249df5f14521424e71b0884c9d846bf74d769
Tyler Cook <tcc@sandpolis.com>
2021-09-11 21:23:00 -0500

    Update platform enums

--- src/main/java/com/sandpolis/core/foundation/util/SystemUtil.java
+++ src/main/java/com/sandpolis/core/foundation/util/SystemUtil.java
@@ -9,12 +9,25 @@
 //============================================================================//
 package com.sandpolis.core.foundation.util;
 
-import static com.sandpolis.core.foundation.Platform.OsType.AIX;
-import static com.sandpolis.core.foundation.Platform.OsType.BSD;
-import static com.sandpolis.core.foundation.Platform.OsType.DARWIN;
-import static com.sandpolis.core.foundation.Platform.OsType.LINUX;
+import static com.sandpolis.core.foundation.Platform.OsType.FREEBSD;
+import static com.sandpolis.core.foundation.Platform.ArchType.AARCH64;
+import static com.sandpolis.core.foundation.Platform.ArchType.ARM;
+import static com.sandpolis.core.foundation.Platform.ArchType.MIPS;
+import static com.sandpolis.core.foundation.Platform.ArchType.MIPS64;
+import static com.sandpolis.core.foundation.Platform.ArchType.POWERPC;
+import static com.sandpolis.core.foundation.Platform.ArchType.POWERPC64;
+import static com.sandpolis.core.foundation.Platform.ArchType.S390X;
+import static com.sandpolis.core.foundation.Platform.ArchType.SPARC64;
+import static com.sandpolis.core.foundation.Platform.ArchType.UNKNOWN_ARCH;
+import static com.sandpolis.core.foundation.Platform.ArchType.X86;
+import static com.sandpolis.core.foundation.Platform.ArchType.X86_64;
+import static com.sandpolis.core.foundation.Platform.OsType.DRAGONFLYBSD;
+import static com.sandpolis.core.foundation.Platform.OsType.NETBSD;
+import static com.sandpolis.core.foundation.Platform.OsType.OPENBSD;
 import static com.sandpolis.core.foundation.Platform.OsType.SOLARIS;
-import static com.sandpolis.core.foundation.Platform.OsType.UNRECOGNIZED;
+import static com.sandpolis.core.foundation.Platform.OsType.LINUX;
+import static com.sandpolis.core.foundation.Platform.OsType.MACOS;
+import static com.sandpolis.core.foundation.Platform.OsType.UNKNOWN_OS;
 import static com.sandpolis.core.foundation.Platform.OsType.WINDOWS;
 
 import java.io.BufferedReader;
@@ -28,6 +41,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.io.CharStreams;
+import com.sandpolis.core.foundation.Platform.ArchType;
 import com.sandpolis.core.foundation.Platform.OsType;
 
 public final class SystemUtil {
@@ -36,33 +50,93 @@ public final class SystemUtil {
 
 	public static final OsType OS_TYPE = queryOsType();
 
+	public static final ArchType ARCH_TYPE = queryArchType();
+
 	/**
-	 * Detect the OS type of the current system.
-	 *
-	 * @return The system's {@link OsType} or {@code UNRECOGNIZED}
+	 * @return The system's {@link OsType}
 	 */
 	private static OsType queryOsType() {
 		String name = System.getProperty("os.name").toLowerCase();
 
-		if (name.startsWith("windows"))
+		if (name.contains("windows"))
 			return WINDOWS;
 
-		if (name.startsWith("linux"))
+		if (name.contains("linux"))
 			return LINUX;
 
-		if (name.startsWith("mac") || name.startsWith("darwin"))
-			return DARWIN;
+		if (name.contains("mac") || name.contains("darwin"))
+			return MACOS;
 
-		if (name.startsWith("solaris") || name.startsWith("sunos"))
+		if (name.contains("solaris") || name.contains("sunos"))
 			return SOLARIS;
 
-		if (name.startsWith("freebsd") || name.startsWith("openbsd"))
-			return BSD;
+		if (name.contains("freebsd"))
+			return FREEBSD;
 
-		if (name.startsWith("aix"))
-			return AIX;
+		if (name.contains("openbsd"))
+			return OPENBSD;
 
-		return UNRECOGNIZED;
+		if (name.contains("netbsd"))
+			return NETBSD;
+
+		if (name.contains("dragonfly"))
+			return DRAGONFLYBSD;
+
+		return UNKNOWN_OS;
+	}
+
+	/**
+	 * @return The system's {@link ArchType}
+	 */
+	private static ArchType queryArchType() {
+
+		// Try uname first because many systems have it
+		String uname = exec("uname", "-m").string().toLowerCase();
+
+		if (!uname.isBlank()) {
+			if (uname.contains("x86_64") || uname.contains("ia64"))
+				return X86_64;
+
+			if (uname.contains("i686") || uname.contains("i386"))
+				return X86;
+
+			if (uname.contains("armv7") || uname.contains("armv6"))
+				return ARM;
+
+			if (uname.contains("aarch64") || uname.contains("armv8"))
+				return AARCH64;
+
+			if (uname.contains("ppc64"))
+				return POWERPC64;
+
+			if (uname.contains("ppc"))
+				return POWERPC;
+
+			if (uname.contains("mips64"))
+				return MIPS64;
+
+			if (uname.contains("mips"))
+				return MIPS;
+
+			if (uname.contains("s390"))
+				return S390X;
+
+			if (uname.contains("sparc"))
+				return SPARC64;
+		}
+
+		// Also try WMI on windows
+		if (OS_TYPE == WINDOWS) {
+			String wmic = exec("wmic", "computersystem", "get", "systemtype").string().toLowerCase();
+
+			if (wmic.contains("x64"))
+				return X86_64;
+
+			if (wmic.contains("x86"))
+				return X86;
+		}
+
+		return UNKNOWN_ARCH;
 	}
 
 	public static final class ProcessWrapper {
